82dfd770b5b080404ab155966bf4451f8669acb3
lidaye <541725460@qq.com>
2019-09-19 20:59:04 +0800

    ini

---
+++ loan-user/src/main/java/com/team/loanuser/LoanUserApplication.java
@@ -0,0 +1,13 @@
+package com.team.loanuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanUserApplication.class, args);
+    }
+
+}
