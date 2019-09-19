15db8be0924b07ac48936fa035c36a2225d217f4
Administrator <5923212328@qq.com>
2019-09-19 20:57:48 +0800

    tt放冰箱

--- loan-order/src/main/java/com/team/loanorder/LoanOrderApplication.java
+++ loan-order/src/main/java/com/team/loanorder/LoanOrderApplication.java
@@ -1,13 +1,13 @@
-package com.vip.loans;
+package com.team.loanorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoansApplication {
+public class LoanOrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LoansApplication.class, args);
+        SpringApplication.run(LoanOrderApplication.class, args);
     }
 
 }
--- loan-order/src/test/java/com/team/loanorder/LoanOrderApplicationTests.java
+++ loan-order/src/test/java/com/team/loanorder/LoanOrderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.vip.loans;
+package com.team.loanorder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LoansApplicationTests {
+public class LoanOrderApplicationTests {
 
     @Test
     public void contextLoads() {
